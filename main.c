4362eb46e7436f554438530617629ce69ef5b0e0
s_baoshan <s_baoshan@163.com>
2025-10-11 15:15:32 +0800

    remove timewheel, but use actual systemtime

--- main.c
+++ main.c
@@ -2,6 +2,7 @@
 #include <stdint.h>
 #include <unistd.h>
 #include <string.h>
+#include <stdlib.h>
 #include "timewheel.h"
 
 #define SCHED_GRANULARITY       (100) /* ms */
@@ -10,52 +11,67 @@ arg_t g_funcArg[17] = { 0 };
 
 void funccc(void *arg)
 {
-    if (arg == NULL) {
+    if (arg == NULL)
+    {
         DEBUG_TIME_LINE("arg: null");
         return;
     }
 
-    arg_t *ev = (arg_t *)arg;
+    arg_t *ev = (arg_t*) arg;
     ev->runCount++;
 
-    ev->expectRunTime = ev->startTime + ev->runCount * ev->interval;
-    uint32_t now = get_local_time_ms();
-    uint32_t diff = now > ev->expectRunTime ? now - ev->expectRunTime : ev->expectRunTime - now;
-    float error = ((float)diff / (float)ev->interval) * 100.0f;
-    DEBUG_TIME_LINE("event[%u] runCount-%u, expectRunTime-%u, now-%u, diff-%u, interval-%u, error-%.2f%%",
-            ev->id, ev->runCount, ev->expectRunTime, now, diff, ev->interval, error);
+    ev->expectTimeMs = ev->startTimeMs + ev->runCount * ev->interval;
+    struct timespec now;
+    clock_gettime(CLOCK_REALTIME, &now);
+    uint64_t nowMs = get_ms_by_timesp(&now);
+    uint64_t diff = nowMs > ev->expectTimeMs ? nowMs - ev->expectTimeMs : ev->expectTimeMs - nowMs;
+    float error = ((float) diff / (float) ev->interval) * 100.0f;
+    DEBUG_TIME_LINE("event[%u] startTime-%llu runCount-%u, expectRunTime-%llu, now-%llu, diff-%llu, interval-%u, error-%.4f%%",
+            ev->id, ev->startTimeMs, ev->runCount, ev->expectTimeMs, nowMs, diff, ev->interval, error);
 }
 
-int main(int argc __attribute__((unused)), char *argv[] __attribute__((unused)))
+int main(int argc, char *argv[])
 {
-    TimeWheel_t *wheel = timewheel_create(SCHED_GRANULARITY, 10);
-    if (wheel == NULL) {
-        fprintf(stderr, "Failed to create timewheel\n");
+    EventList_t *eventList = (EventList_t*) calloc(1, sizeof(EventList_t));
+    if (eventList == NULL)
+    {
+        fprintf(stderr, "Failed to calloc eventList\n");
         return -1;
     }
 
-    printf("TimeWheel created successfully\n");
+    if (eventListInit(eventList) < 0)
+    {
+        free(eventList);
+        fprintf(stderr, "Failed to init eventList\n");
+        return -1;
+    }
+
+    printf("eventList created successfully\n");
     printf("Creating %lu timing events...\n", ARRAY_SIZE(g_funcArg));
 
-    for (uint32_t i = 0; i < ARRAY_SIZE(g_funcArg); i++) {
+    struct timespec now;
+    for (uint32_t i = 0; i < ARRAY_SIZE(g_funcArg); i++)
+    {
         g_funcArg[i].id = i + 1;
         g_funcArg[i].val = SCHED_GRANULARITY * (i + 1);
         g_funcArg[i].interval = SCHED_GRANULARITY * (i + 1);
-        g_funcArg[i].startTime = get_local_time_ms();
-        
-        if (timewheel_create_event(wheel, SCHED_GRANULARITY * (i + 1), funccc, &g_funcArg[i]) != 0) {
-            fprintf(stderr, "Failed to create event %u\n", i + 1);
-        }
+
+        clock_gettime(CLOCK_REALTIME, &now);
+        g_funcArg[i].startTimeMs = get_ms_by_timesp(&now);
+        g_funcArg[i].nextTimemMs = g_funcArg[i].startTimeMs + g_funcArg[i].interval;
+
+        eventList_addEvent(eventList, g_funcArg[i].interval, funccc, &g_funcArg[i]);
     }
 
     printf("All events created. Running...\n");
 
-    while (1) {
+    while (1)
+    {
         sleep(1);
     }
 
     /* Cleanup (never reached in this example) */
-    timewheel_destroy(wheel);
+    eventList_destroy(eventList);
 
     return 0;
 }
--- timewheel.h
+++ timewheel.h
@@ -13,62 +13,70 @@
 
 /* Time position in the wheel */
 typedef struct TimePos {
-    uint32_t pos_ms;
-    uint32_t pos_sec;
-    uint32_t pos_min;
+        uint32_t pos_ms;
+        uint32_t pos_sec;
+        uint32_t pos_min;
 } TimePos_t;
 
-/* Event callback function type */
-typedef void (*EventCallback_t)(void*);
+typedef void (*EventCallback_t)(void*); //Event callback function type
+typedef void (*freeCallback_t)(void*);  //Free argument callback function type
 
 /* Event argument structure */
 typedef struct eventArg {
-    uint32_t startTime;
-    uint32_t expectRunTime;
-    uint32_t runCount;
-    uint32_t id;
-    uint32_t val;
-    uint32_t interval;
+        uint64_t startTimeMs;
+        uint64_t nextTimemMs;
+        uint64_t expectTimeMs;
+        uint32_t runCount;
+        uint32_t id;
+        uint32_t val;
+        uint32_t interval;              // interval in milliseconds
 } arg_t;
 
 /* Event structure */
 typedef struct Event {
-    uint32_t id;
-    EventCallback_t cb;
-    void *arg;
-    TimePos_t timePos;
-    uint32_t interval;
-    struct Event *next;  /* for linked list */
+        uint32_t id;
+        EventCallback_t cb;
+        arg_t *arg;
+        freeCallback_t freeArgCb;
+        TimePos_t timePos;
+        uint32_t interval;
+        struct Event *next; /* for linked list */
 } Event_t;
 
 /* Event list node (linked list) */
 typedef struct EventList {
-    Event_t *head;
-    Event_t *tail;
-    uint32_t count;
+        Event_t *head;
+        Event_t *tail;
+        uint32_t count;
+        pthread_t loopThread;
+        pthread_mutex_t mutex;
 } EventList_t;
 
 /* Event slot array */
 typedef struct EventSlotArray {
-    EventList_t *slots;
-    uint32_t size;
+        EventList_t *slots;
+        uint32_t size;
 } EventSlotArray_t;
 
 /* TimeWheel structure */
 typedef struct TimeWheel {
-    EventSlotArray_t eventSlotArray;    /* event slot array */
-    TimePos_t timePos;                   /* current time position of wheel */
-    pthread_t loopThread;                /* thread for loop */
-    
-    uint32_t firstLevelCount;            /* millisecond level */
-    uint32_t secondLevelCount;           /* second level, fixed to 60 */
-    uint32_t thirdLevelCount;            /* minute level */
-    
-    uint32_t steps;                      /* milliseconds of one tick */
-    uint32_t increaseId;                 /* event id increase number */
-    pthread_mutex_t mutex;               /* mutex for event slot list */
+        EventList_t eventList;
+        EventSlotArray_t eventSlotArray; /* event slot array */
+        TimePos_t timePos; /* current time position of wheel */
+        pthread_t loopThread; /* thread for loop */
+
+        uint32_t firstLevelCount; /* millisecond level */
+        uint32_t secondLevelCount; /* second level, fixed to 60 */
+        uint32_t thirdLevelCount; /* minute level */
+
+        uint32_t steps; /* milliseconds of one tick */
+        uint32_t increaseId; /* event id increase number */
+        pthread_mutex_t mutex; /* mutex for event slot list */
 } TimeWheel_t;
 
+int eventListInit(EventList_t *eventList);
+int eventList_addEvent(EventList_t *eventList, uint32_t interval, EventCallback_t callback, void *arg);
+void eventList_destroy(EventList_t *eventList);
 /* Public API functions */
 TimeWheel_t* timewheel_create(uint32_t steps, uint32_t maxMin);
 void timewheel_destroy(TimeWheel_t *wheel);
@@ -76,7 +84,7 @@ int timewheel_init(TimeWheel_t *wheel, uint32_t steps, uint32_t maxMin);
 int timewheel_create_event(TimeWheel_t *wheel, uint32_t interval, EventCallback_t callback, void *arg);
 
 /* Utility functions */
-uint32_t get_local_time_ms(void);
+uint64_t get_ms_by_timesp(struct timespec *tp);
 void get_local_time(char *buf, uint32_t bufLen);
 void debugBufFormat2fp(FILE *fp, const char *file, const char *func,
         int line, char *buf, int len, const char *fmt, ...);
